d2a9d65f55da9701b5006650b65776377c9b4e7e
Streel <maximilian.wistedt@gmail.com>
2023-08-05 18:15:21 +0200

    Trees
    First comitt

---
+++ src/main/java/net/streel/treesreplant/TreesReplant.java
@@ -0,0 +1,17 @@
+package net.streel.treesreplant;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TreesReplant implements ModInitializer {
+	public static final String MOD_ID="treesreplant";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/streel/treesreplant/TreesReplantClient.java
@@ -0,0 +1,11 @@
+package net.streel.treesreplant;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TreesReplantClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
